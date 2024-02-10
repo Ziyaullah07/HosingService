314d0f258c3e3c584dd154e07e2bd703b08791d5
Ziyaullah07 <ziyaullah07@gmail.com>
2024-02-10 18:05:32 +0530

    First commit

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+selector: "app-property-card",
+//template:`<h1>property-Card component</h1>`,
+templateUrl:'property-card.component.html',
+//styles:['h1{font-weight:normal;}']
+styleUrls:['property-card.component.css']
+})
+export class PropertyCardComponent{
+ Property : any = {
+    "Id":"1",
+    "Name": "Ziyaullah",
+    "Price": 200
+
+ }
+}
